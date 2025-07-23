bed19611301146224661e087bd505bfea458b276
Joven Soh <34702990+JovenSoh@users.noreply.github.com>
2025-07-23 11:02:05 +0800

    Update guidance

--- benchmarks/stable_diffusion.py
+++ benchmarks/stable_diffusion.py
@@ -264,7 +264,8 @@ if __name__ == "__main__":
         # encode contexts
         ids = tokenizer.encode(prompt_text)
         context = model.cond_stage_model.transformer.text_model(Tensor([ids])).realize()
-        uncond = model.cond_stage_model.transformer.text_model(Tensor([[ ]])).realize()  # empty prompt
+        uncond_ids = tokenizer.encode(" ") 
+        uncond = model.cond_stage_model.transformer.text_model(Tensor([[uncond_ids]])).realize()  # empty prompt
 
         if args.seed is not None:
             Tensor.manual_seed(args.seed + idx)
